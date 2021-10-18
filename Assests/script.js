3b0f71ac4e3790834a42f4fe97d49a0063f58c58
Chase Rivera <achaserivera@gmail.com>
2021-10-18 12:47:05 -0700

    built out function to call data from api for daily weather, location and created colorcoded UV index. once data is received it will render to corresponding display elements

--- Assests/script.js
+++ Assests/script.js
@@ -15,3 +15,78 @@ function renderSearchHistory() {
         `);
     }
 }
+
+function initPage() {
+    if (JSON.parse(localStorage.getItem("searchHistory")) !== null) {
+        renderSearchHistory();
+    }
+}
+
+function dispWeather(location) {
+    fetch(genWeatherQueryURL(location))
+        .then((response) => {
+            if (!response.ok) {
+                throw response.json();
+            }
+            return response.json();
+        })
+        .then((weatherData) => {
+            fetch(genOneCallQueryURL(weatherData.coord.lat, weatherData.coord.lon))
+              .then((response) => {
+                  if (!response.ok) {
+                      throw response.json();
+                  }
+                  return response.json();
+              })
+              .then((oneCallData) => {
+                  var uviColor;
+                $("#dayAndWeekForecast").empty();
+                if (oneCallData.current.uvi <= 2.99) {
+                    uviColor = "bg-success text-white";
+                } else if (oneCallData.current.uvi >= 3 && oneCallData.current.uvi <= 5.99) {
+                    uviColor = "bg-warning";
+                } else if (oneCallData.current.uvi >= 6 && oneCallData.current.uvi <= 7.99) {
+                    uviColor = "bg-orange";
+                } else if (oneCallData.current.uvi >= 8 && oneCallData.current.uvi <= 10.99) {
+                    uviColor = "bg-danger text-white";
+                } else if (oneCallData.current.uvi >= 11) {
+                    uviColor = "bg-warning text-white";
+                }
+                
+                $("#dayAndWeekForecast").append(`
+                    <section id="dailyForecast" class="border border-1 border-dark p-2">
+                        <div class="d-flex flex-row">
+                        <h3 class="fs-3 fw-bold">${weatherData.name + " (" + moment.unix(weatherData.dt).format("l") + " )"}</h2>
+                        <img src="${genIconURL(weatherData.weather[0].icon)}" alt="${weatherData.weather[0].description}">
+                        </div>
+                        <div class="mt-3 d-grid gap-1">
+                        <p>Temp: ${weatherData.main.temp} F</p>
+                        <p>Wind: ${weatherData.wind.speed} MPH</p>
+                        <p>Humidity: ${weatherData.main.humidity} %<p>
+                        <p>UV Index: <span class="p-1 px-3 rounded ${uviColor}">${oneCallData.current.uvi}</span></p>
+                        </div>
+                    </section>
+                    <section class="mt-4">
+                        <h2 class="fs-5 fw-bold>Weekly Forecast:</h2>
+                        <div id=weeklyForecast" class="d-flex flex-row justify-content-between"></div>
+                    </section>
+                `);
+
+                for (i=1;i<6;i++) {
+                    $("#weeklyForecast").append(`
+                    <div id="forecastCard" class="p-2">
+                        <h3>${moment.unix(oneCallData.daily[i].dt).format("l")}</h4>
+                        <img src="${genIconURL(oneCallData.daily[i].weather[0].icon)}" alt="${oneCallData.daily[i].weather.description}">
+                        <div class="mt-2">
+                            <p>Temp: ${oneCallData.daily[i].temp.day} F</p>
+                            <p>Wind: ${oneCallData.daily[i].wind_speed} MPH</p>
+                            <p>Humidity: ${oneCallData.daily[i].humidity} %</p>
+                        </div>
+                    </div>
+                    `)
+                }
+              })  
+        })
+}
+
+initPage();
